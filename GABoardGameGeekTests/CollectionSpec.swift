9164e97acf932f4e7ddc07922fa09ebd2710b56b
Geoff Amey <gca3020@users.noreply.github.com>
2016-04-16 14:51:37 -0400

    Add additional unit tests for games with statistics blocks.

--- GABoardGameGeekTests/CollectionSpec.swift
+++ GABoardGameGeekTests/CollectionSpec.swift
@@ -63,6 +63,71 @@ class CollectionSpec: QuickSpec {
                 }
             }
 
+            context("from a brief xml with statistics") {
+                let xml =
+                    "<item objecttype=\"thing\" objectid=\"222\" subtype=\"boardgame\" collid=\"22\">" +
+                    "    <name sortindex=\"3\">A Brief Game With Stats</name>" +
+                    "    <stats minplayers=\"4\" maxplayers=\"8\" minplaytime=\"120\" maxplaytime=\"480\" playingtime=\"240\" numowned=\"2222\">" +
+                    "        <rating value=\"8.5\">" +
+                    "            <average value=\"7.79921\"/>" +
+                    "            <bayesaverage value=\"7.58107\"/>" +
+                    "        </rating>" +
+                    "    </stats>" +
+                    "    <status own=\"0\" prevowned=\"0\" fortrade=\"0\" want=\"0\" wanttoplay=\"1\" wanttobuy=\"0\" wishlist=\"1\" wishlistpriority=\"3\" preordered=\"0\" lastmodified=\"2015-12-18 09:38:29\"/>" +
+                    "</item>"
+
+                beforeEach {
+                    parser = SWXMLHash.parse(xml)
+                    game = try! parser!["item"].value()
+                }
+
+                it("should have an objectId") {
+                    expect(game!.objectId).to(equal(222))
+                }
+
+                it("should have a name") {
+                    expect(game!.name).to(equal("A Brief Game With Stats"))
+                    expect(game!.sortName).to(equal("Brief Game With Stats"))
+                }
+
+                it("should have a status block where wanttoplay and wishlist are true and wishlist priority is set") {
+                    expect(game!.status.owned).to(equal(false))
+                    expect(game!.status.prevOwned).to(equal(false))
+                    expect(game!.status.forTrade).to(equal(false))
+                    expect(game!.status.wantInTrade).to(equal(false))
+                    expect(game!.status.wantToPlay).to(equal(true))
+                    expect(game!.status.wantToBuy).to(equal(false))
+                    expect(game!.status.wishList).to(equal(true))
+                    expect(game!.status.preOrdered).to(equal(false))
+                    expect(game!.status.wishListPriority).to(equal(3))
+                }
+
+                it("should have a simple stats block") {
+                    expect(game!.stats).toNot(beNil())
+                    let stats = game!.stats!
+                    expect(stats.minPlayers).to(equal(4))
+                    expect(stats.maxPlayers).to(equal(8))
+                    expect(stats.minPlaytime).to(equal(120))
+                    expect(stats.maxPlaytime).to(equal(480))
+                    expect(stats.playingTime).to(equal(240))
+                    expect(stats.numOwned).to(equal(2222))
+
+                    expect(stats.rating.userRating).to(beCloseTo(8.5))
+                    expect(stats.rating.averageRating).to(beCloseTo(7.79921))
+                    expect(stats.rating.bayesAverageRating).to(beCloseTo(7.58107))
+                }
+
+                it("should not have any other optional values populated") {
+                    expect(game!.imagePath).to(beNil())
+                    expect(game!.thumbnailPath).to(beNil())
+                    expect(game!.wishListComment).to(beNil())
+                    expect(game!.comment).to(beNil())
+                    expect(game!.numPlays).to(beNil())
+                    expect(game!.yearPublished).to(beNil())
+                    expect(game!.comment).to(beNil())
+                }
+            }
+
             context("from a standard xml") {
 
                 let xml =
@@ -130,6 +195,122 @@ class CollectionSpec: QuickSpec {
                 }
             }
 
+            context("from a standard xml with statistics") {
+
+                let xml =
+                    "<item objecttype=\"thing\" objectid=\"444\" subtype=\"boardgame\" collid=\"44\">" +
+                    "    <name sortindex=\"3\">A Game With Stats</name>" +
+                    "    <yearpublished>2016</yearpublished>" +
+                    "    <image>//path.to/image.jpg</image>" +
+                    "    <thumbnail>//path.to/thumbnail.jpg</thumbnail>" +
+                    "    <stats minplayers=\"1\" maxplayers=\"2\" minplaytime=\"20\" maxplaytime=\"60\" playingtime=\"45\" numowned=\"4444\">" +
+                    "        <rating value=\"N/A\">" +
+                    "            <usersrated value=\"70\"/>" +
+                    "            <average value=\"7.66143\"/>" +
+                    "            <bayesaverage value=\"5.68734\"/>" +
+                    "            <stddev value=\"0.795755\"/>" +
+                    "            <median value=\"0\"/>" +
+                    "            <ranks>" +
+                    "                <rank type=\"subtype\" id=\"1\" name=\"boardgame\" friendlyname=\"Board Game Rank\" value=\"3785\" bayesaverage=\"5.68734\"/>" +
+                    "                <rank type=\"family\" id=\"5497\" name=\"strategygames\" friendlyname=\"Strategy Game Rank\" value=\"20\" bayesaverage=\"7.74838\"/>" +
+                    "            </ranks>" +
+                    "        </rating>" +
+                    "    </stats>" +
+                    "    <status own=\"1\" prevowned=\"0\" fortrade=\"1\" want=\"0\" wanttoplay=\"0\" wanttobuy=\"0\" wishlist=\"0\" preordered=\"0\" lastmodified=\"2016-04-04 20:19:37\"/>" +
+                    "    <numplays>2</numplays>" +
+                    "    <wishlistcomment>" +
+                    "        Wish List Comment" +
+                    "    </wishlistcomment>" +
+                    "</item>"
+
+                beforeEach {
+                    parser = SWXMLHash.parse(xml)
+                    game = try! parser!["item"].value()
+                }
+
+                it("should have an objectId") {
+                    expect(game!.objectId).to(equal(444))
+                }
+
+                it("should have a name") {
+                    expect(game!.name).to(equal("A Game With Stats"))
+                    expect(game!.sortName).to(equal("Game With Stats"))
+                }
+
+                it("should have a status block where owned and fortrade is true") {
+                    expect(game!.status.owned).to(equal(true))
+                    expect(game!.status.prevOwned).to(equal(false))
+                    expect(game!.status.forTrade).to(equal(true))
+                    expect(game!.status.wantInTrade).to(equal(false))
+                    expect(game!.status.wantToPlay).to(equal(false))
+                    expect(game!.status.wantToBuy).to(equal(false))
+                    expect(game!.status.wishList).to(equal(false))
+                    expect(game!.status.preOrdered).to(equal(false))
+                    expect(game!.status.wishListPriority).to(beNil())
+                }
+
+                it("should have a publication year") {
+                    expect(game!.yearPublished).to(equal(2016))
+                }
+
+                it("should have an image and thumbnail URL") {
+                    expect(game!.imagePath).to(equal("//path.to/image.jpg"))
+                    expect(game!.thumbnailPath).to(equal("//path.to/thumbnail.jpg"))
+                }
+
+                it("should have plays logged") {
+                    expect(game!.numPlays).to(equal(2))
+                }
+
+                it("should not have a comment") {
+                    expect(game!.comment).to(beNil())
+                }
+
+                it("should have a wishlist comment") {
+                    expect(game!.wishListComment).to(equal("Wish List Comment"))
+                }
+
+                it("should have a statistics block") {
+                    expect(game!.stats).toNot(beNil())
+                    let stats = game!.stats!
+                    expect(stats.minPlayers).to(equal(1))
+                    expect(stats.maxPlayers).to(equal(2))
+                    expect(stats.minPlaytime).to(equal(20))
+                    expect(stats.maxPlaytime).to(equal(60))
+                    expect(stats.playingTime).to(equal(45))
+                    expect(stats.numOwned).to(equal(4444))
+                }
+
+                it("should have a rating block") {
+                    expect(game!.stats!.rating).toNot(beNil())
+                    let rating = game!.stats!.rating
+                    expect(rating.userRating).to(beNil())
+                    expect(rating.usersRated).to(equal(70))
+                    expect(rating.averageRating).to(beCloseTo(7.66143))
+                    expect(rating.bayesAverageRating).to(beCloseTo(5.68734))
+                    expect(rating.stdDev).to(beCloseTo(0.795755))
+                    expect(rating.median).to(beCloseTo(0.0))
+                }
+
+                it("should have ranks") {
+                    expect(game!.stats!.rating.ranks).toNot(beNil())
+                    let ranks = game!.stats!.rating.ranks!
+                    expect(ranks.count).to(equal(2))
+                    expect(ranks[0].type).to(equal("subtype"))
+                    expect(ranks[0].id).to(equal(1))
+                    expect(ranks[0].name).to(equal("boardgame"))
+                    expect(ranks[0].friendlyName).to(equal("Board Game Rank"))
+                    expect(ranks[0].value).to(equal(3785))
+                    expect(ranks[0].bayesAverage).to(beCloseTo(5.68734))
+
+                    expect(ranks[1].type).to(equal("family"))
+                    expect(ranks[1].id).to(equal(5497))
+                    expect(ranks[1].name).to(equal("strategygames"))
+                    expect(ranks[1].friendlyName).to(equal("Strategy Game Rank"))
+                    expect(ranks[1].value).to(equal(20))
+                    expect(ranks[1].bayesAverage).to(beCloseTo(7.74838))
+                }
+            }
 
         }
     }
