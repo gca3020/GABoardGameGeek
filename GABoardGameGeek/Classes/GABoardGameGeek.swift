fe2cd7edc47c36658302a112b8b83a44593f8d6c
Geoff Amey <gca3020@users.noreply.github.com>
2016-06-11 10:05:05 -0400

    Some additional genericizing, and combining the NetworkAdapter and XmlAdapter into a single ApiAdapter.

--- GABoardGameGeek/Classes/GABoardGameGeek.swift
+++ GABoardGameGeek/Classes/GABoardGameGeek.swift
@@ -32,9 +32,7 @@ public class GABoardGameGeek {
 
         // Make the network call to get the collection.
         let endTime = NSDate().dateByAddingTimeInterval(Double(timeoutSeconds))
-        collectionRequest(requestParams, retryUntil: endTime) { result in
-            closure(result)
-        }
+        api.request(self.collectionUrl, params: requestParams, rootElement: "items", childElement: "item", retryUntil: endTime, closure: closure)
     }
 
     /**
@@ -52,14 +50,7 @@ public class GABoardGameGeek {
         requestParams["stats"] = (stats ? "1" : "0")
 
         // Make the request
-        networkAdapter.requestData(self.itemUrl, params: requestParams) { result in
-            switch(result) {
-            case .Success(let resultString):
-                closure(self.xmlAdapter.parse(resultString, rootElement: "items", childElement: "item"))
-            case .Failure(let error):
-                closure(.Failure(error))
-            }
-        }
+        api.request(self.itemUrl, params: requestParams, rootElement: "items", childElement: "item", closure: closure)
     }
 
     /**
@@ -87,55 +78,22 @@ public class GABoardGameGeek {
     }
 
     // MARK: - Initializer
-    init() {
-        self.networkAdapter = NetworkAdapter()
-        self.xmlAdapter = XmlAdapter()
-    }
-
-    // MARK: - Private Functions
 
     /**
-     Make an API request specifically for a user collection. This function exists separately from the
-     user callable function, since this will potentially be called multiple times while retrying.
+     Initialize the GABoardGameGeek API
 
-     - parameter params:     The parameters of the Collection Request, already encoded as a URL
-     - parameter retryUntil: The absolute end time after which we should stop retrying.
-     - parameter closure:    The completion closure when we have either finished or failed
+     - returns: A GABoardGameGeek class ready to handle API requests
      */
-    private func collectionRequest(params: [String: String], retryUntil: NSDate, closure: ApiResult<[CollectionBoardGame]> -> ()) {
-        // Make the raw request, and handle the result
-        networkAdapter.requestData(self.collectionUrl, params: params) { result in
-            switch(result) {
-            case .Success(let xmlString):
-                closure(self.xmlAdapter.parse(xmlString, rootElement: "items", childElement: "item"))
-            case .Failure(let error):
-                switch(error) {
-                case .ServerNotReady:
-                    // Special case for Collection Requests, a 202 status code means we should try again,
-                    // so queue up a retry to happen in one second, as long as we're still below the timeout.
-                    if( (NSDate().compare(retryUntil) == NSComparisonResult.OrderedAscending) ) {
-                        let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(1.0 * Double(NSEC_PER_SEC)))
-                        dispatch_after(delay, dispatch_get_main_queue()) {
-                            self.collectionRequest(params, retryUntil: retryUntil, closure: closure)
-                        }
-                    }
-                    else {
-                        closure(.Failure(error))
-                    }
-                default:
-                    closure(.Failure(error))
-                }
-            }
-        }
+    init() {
+        self.api = ApiAdapter()
     }
 
     // MARK: - Private Member Variables
 
-    /// A Network Adapter to separate out all of the networking functionality of the API
-    private let networkAdapter: NetworkAdapter
+    /// An API Adapter to separate out all of the internal Networking and XML Parsing Functionality
+    private let api: ApiAdapter
 
-    /// An XML Adapter to separate out all of the XML parsing code
-    private let xmlAdapter: XmlAdapter
+    // MARK: - Constants
 
     /// The BGG URL to use when querying a user's collection
     private let collectionUrl = "https://boardgamegeek.com/xmlapi2/collection"
